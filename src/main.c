bdadefdd6e950c5bea0f00349321e82bd3175219
Apeksha Vinayak Bodade <apeksha@7yb-vf.local>
2025-06-13 15:47:07 -0700

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -5,10 +5,14 @@
 #include <unistd.h>
 // #include <sys/types.h>
 #include <sys/wait.h>
+#include <ctype.h>
+
 #define MAX_BUFFER_SIZE 128
 #define NUM_BUILTINS 4
 #define PATH_MAX 100
-char *builtins[NUM_BUILTINS] = {"echo", "exit", "type", "pwd"};
+#define MAX_COMMANDS 100
+
+char *builtins[NUM_BUILTINS] = {"echo", "exit", "type", "pwd", "cd"};
 
 typedef struct {
   char *input;
@@ -45,9 +49,6 @@ bool search_builtin(const char* command) {
     // Get the next token
     dir = strtok(NULL, ":");
   }
-  // if (!found) {
-  //   printf("%s: not found\n", command);
-  // }
   free(path_copy);
   return found;
 }
@@ -56,7 +57,7 @@ bool is_valid(char* path, int path_len) {
 
 }
 
-void handleType(InputBuffer *input_buffer) {
+void handleType() {
   char *command2 = strtok(NULL, "");
   for(int i=0; i<NUM_BUILTINS; i++) {
     if(strncmp(command2, builtins[i], strlen(command2)) == 0) {
@@ -72,29 +73,31 @@ void handleType(InputBuffer *input_buffer) {
   // If we reach here, the command is not a builtin
   printf("%s: not found\n", command2);
 }
-
-bool process_input(InputBuffer *input_buffer, char *command) {
+bool process_input(char* input_buffer, char* command) {
+  // char *command = strtok(input_buffer, " ");
+  // printf("input_buffer = %s command = %s", input_buffer, command);
+// bool process_input(InputBuffer *input_buffer, char *command) {
   if(command == NULL) {
-    input_buffer->is_valid = true; // No command entered, mark as valid
+    // input_buffer->is_valid = true; // No command entered, mark as valid
     return true;
   }
   
   if(strncmp(command, "exit", 4) == 0) {
-    input_buffer->is_valid = true;
+    // input_buffer->is_valid = true;
     exit(EXIT_SUCCESS);
   } else if(strncmp(command, "echo", 4) == 0) {
-    input_buffer->is_valid = true;
-    printf("%s\n", input_buffer->input + strlen(command) + 1); // +1 to skip the space after "echo"
+    // input_buffer->is_valid = true;
+    printf("%s\n", input_buffer + strlen(command) + 1); // +1 to skip the space after "echo"
     return true;
   } else if(strncmp(command, "type", 4) == 0) {
-    handleType(input_buffer);
-    input_buffer->is_valid = true;
+    handleType();
+    // input_buffer->is_valid = true;
     return true;
   } else if(strcmp(command, "pwd") == 0) {
     char cwd[PATH_MAX];
     if(getcwd(cwd, sizeof(cwd)) != NULL) {
       printf("%s\n", cwd);
-      input_buffer->is_valid = true;
+      // input_buffer->is_valid = true;
       return true;
     }
   } else if(strcmp(command, "cd") == 0) {
@@ -102,16 +105,97 @@ bool process_input(InputBuffer *input_buffer, char *command) {
     if(path == NULL || strcmp(path, "~") == 0) {
       // should go to home path
       chdir(getenv("HOME"));
-      input_buffer->is_valid = true;
+      // input_buffer->is_valid = true;
       return true;
     }
     if(chdir(path) != 0) {
       printf("cd: %s: No such file or directory\n", path);
     }
-    input_buffer->is_valid = true;
+    // input_buffer->is_valid = true;
     return true;
   }
+  // printf("returning false for command = %s", command);
   return false;
+}
+
+char* trim(char* token) {
+  while (isspace(*token)) {   // ctype.h
+    token++;
+  }
+  if (*token == 0) {
+    return token;
+  }
+
+  char *end = token + strlen(token) - 1;
+  while (end > token && isspace(*end)) {
+    end--;
+  }
+  *(end + 1) = '\0';
+  return token;
+}
+
+char** getPipedCommands(char* input_buffer, int *num_cmds) {
+  char **cmds = malloc(MAX_COMMANDS * sizeof(char *));
+  if (!cmds) {
+    perror("malloc failed");
+    exit(EXIT_FAILURE);
+  }
+  int ind = 0;
+  char *token = strtok(input_buffer, "|");
+  while(token != NULL) {
+    cmds[ind++] = trim(token);
+    token = strtok(NULL, "|");
+  }
+  *num_cmds = ind;
+  return cmds;
+}
+
+void processCommands(char *input_buffer, bool toFork) {
+  // printf("processCommands = %s\n", input_buffer);
+  // process_input(&input_buffer, command);
+  // if(!input_buffer.is_valid) {
+  char *command = strtok(input_buffer, " ");
+  if(!process_input(input_buffer, command)) {
+    // printf("came to fork and execute....%s", input_buffer);
+    
+    // Parse the input into arguments
+    char *args[MAX_BUFFER_SIZE / 2 + 1];    // considering string/argument length atleast 2 characters
+    int arg_count = 1;
+    args[0] = command; // First argument is the command itself
+    // char *token = strtok(input_buffer.input, " ");
+    char* token = strtok(NULL, " ");
+    while(token != NULL && arg_count < (MAX_BUFFER_SIZE / 2)) {
+      args[arg_count] = token;
+      arg_count++;
+      token = strtok(NULL, " ");
+    }
+    args[arg_count] = NULL; // Null-terminate the array of arguments
+
+    if(args[0] == NULL) {
+      // printf("$ ");
+      // continue; // No command entered, prompt again
+      return;
+    }
+    if(toFork) {
+      pid_t pid = fork();
+      if(pid == 0) {
+        // Child Process
+        execvp(args[0], args);
+        printf("%s: command not found\n", args[0]);
+        exit(EXIT_FAILURE); // Exit child process if exec fails
+      } else if(pid > 0) {
+        // Parent Process
+        int status;
+        waitpid(pid, &status, 0);
+      } else {
+        perror("fork");
+      }
+    } else {
+      execvp(args[0], args);
+      perror("execvp failed");
+      exit(EXIT_FAILURE);
+    }
+  }
 }
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -134,48 +218,61 @@ int main(int argc, char *argv[]) {
     input_buffer.input_length = strlen(input_buffer.input);
     input_buffer.input[input_buffer.input_length - 1] = '\0'; // Remove the trailing newline
     input_buffer.is_valid = false;
-    
-    char *command = strtok(input_buffer.input, " ");
 
-    process_input(&input_buffer, command);
-    if(!input_buffer.is_valid) {
-      // Parse the input into arguments
-      char *args[MAX_BUFFER_SIZE / 2 + 1];    // considering string/argument length atleast 2 characters
-      int arg_count = 1;
-      args[0] = command; // First argument is the command itself
-      // char *token = strtok(input_buffer.input, " ");
-      char* token = strtok(NULL, " ");
-      while(token != NULL && arg_count < (MAX_BUFFER_SIZE / 2)) {
-        
-        args[arg_count] = token;
-        arg_count++;
-        // printf("Token: %s\n", token); // Debugging line to see the tokens
-        // printf("input_buffer.input: %s\n", input_buffer.input); // Debugging line to see the input buffer
-        token = strtok(NULL, " ");
-      }
-      // printf("Number of arguments: %d\n", arg_count); // Debugging line to see the number of arguments
-      args[arg_count] = NULL; // Null-terminate the array of arguments
-
-      if(args[0] == NULL) {
-        printf("$ ");
-        continue; // No command entered, prompt again
-      }
-      pid_t pid = fork();
-      if(pid == 0) {
-        // Child Process
-        execvp(args[0], args);
-        printf("%s: command not found\n", args[0]);
-        exit(EXIT_FAILURE); // Exit child process if exec fails
-      } else if(pid > 0) {
-        // Parent Process
-        int status;
-        waitpid(pid, &status, 0);
-      } else {
-        perror("fork");
+    if(strchr(input_buffer.input, '|') != NULL) {
+      int num_cmds = 0;
+      char **commands = getPipedCommands(input_buffer.input, &num_cmds);
+      // printf("num_cmds = %d", num_cmds);
+      // for(int i=0; i<num_cmds; i++) {
+      //   printf("cmd[%d] = %s", i, commands[i]);
+      // }
+      int pipefd[2 * (num_cmds-1)];
+      //create the necessary pipes, num_cmds-1
+      for(int i=0; i<num_cmds-1; i++) {
+        if(pipe(pipefd + i*2) < 0) {
+          perror("pipe");
+          exit(EXIT_FAILURE);
+        }
       }
 
-      // printf("%s: command not found\n", input_buffer.input);
+      // now fork the processes
+      for(int i=0; i<num_cmds; i++) {
+        // printf("forking for comd[%d] = %s\n", i, commands[i]);
+        pid_t pid = fork();
+        if(pid == 0) {
+          // child process
+          if(i != 0) {
+            dup2(pipefd[(i - 1)*2], STDIN_FILENO);
+          }
+          if(i != num_cmds-1) {
+            dup2(pipefd[i*2 + 1], STDOUT_FILENO);
+          }
+
+          //closing all the pipes
+          for(int j=0; j<(num_cmds-1)*2; j++) {
+            close(pipefd[j]);
+          }
+
+          // call the actual execution with commands[i]
+          // printf("calling process comand for i=%d\n", i);
+          processCommands(commands[i], false);
+        }
+      }
+
+      // closing all the pipes in the parent
+      for(int i=0; i<(num_cmds-1)*2; i++) {
+        close(pipefd[i]);
+      }
+
+      // wait for all the children
+      for(int i=0; i<num_cmds; i++) {
+        wait(NULL);
+      }
+    } else {
+      processCommands(input_buffer.input, true);
     }
+    
+    
     printf("$ ");
   }
   return 0;
